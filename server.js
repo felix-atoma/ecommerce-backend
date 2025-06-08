aef621a976734b51d2265af4eb8feee50f582411
felix-atoma <felixatoma2@gmail.com>
2025-06-08 10:39:59 +0000

    updated server

--- server.js
+++ server.js
@@ -6,19 +6,22 @@ const startServer = async () => {
   try {
     await connectDB();
 
+    // 1. Root health check route
+    app.get('/', (req, res) => {
+      res.send('API is running!');
+    });
+
     const server = app.listen(config.port, () => {
       console.log(`Server running in ${config.env} mode on port ${config.port}`);
     });
 
-    // Handle unhandled promise rejections
-    process.on('unhandledRejection', (err) => {
+    process.on('unhandledRejection', err => {
       console.log('UNHANDLED REJECTION! Shutting down...');
       console.log(err.name, err.message);
       server.close(() => process.exit(1));
     });
 
-    // Handle uncaught exceptions
-    process.on('uncaughtException', (err) => {
+    process.on('uncaughtException', err => {
       console.log('UNCAUGHT EXCEPTION! Shutting down...');
       console.log(err.name, err.message);
       process.exit(1);
